e46252f7ae0db781e66c8003181ad8b7efd0c276
justaryadi <aryadiofficial05@gmail.com>
2026-06-28 19:58:02 +0700

    Update main.jsx for pomodoro

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App_achievement.jsx'
-import App from './App_pomodoro.jsx'
+import App from './App_achievement.jsx'
+// import App from './App_pomodoro.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
